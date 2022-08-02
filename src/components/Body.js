c8d1acad8a8f3a9d81743e19509da5f63dfe14aa
Sarang Pidadi <sarangpidadi07@gmail.com>
2022-08-02 14:20:05 +0530

    ico updated and warnings removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Body.css';
 import DogBreed from './OptionComponents/DogBreed';
 
@@ -6,7 +6,7 @@ const Body = (args) => {
     return (
         <div>
             <div className='bg-dark margin-top'>
-                <h1 className='fw-bold text-white p-2'>Thunderpaws</h1>
+                <h1 className='fw-bold text-white p-3'>Thunderpaws</h1>
                 <h5 className='text-white'>Basically a Dog essential. Explore everything about four legged angel.</h5>
             </div>
             <div className='col-md-12'>
